3410615c51799b933baa94765e70eac5d5a16d47
lindabettini <linda.bettini@hotmail.it>
2022-02-14 23:24:46 +0100

    Add dinamic info in list item

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,13 +3,23 @@
 		<section v-if="movies.length" id="movies-list">
 			<h2>Movies</h2>
 			<ul>
-				<li v-for="movie in movies" :key="movie.id">{{ movie.title }}</li>
+				<li class="card" v-for="movie in movies" :key="movie.id">
+					<div>{{ movie.title }}</div>
+					<div>{{ movie.original_title }}</div>
+					<div>{{ movie.original_language }}</div>
+					<div>{{ movie.vote_average }}</div>
+				</li>
 			</ul>
 		</section>
 		<section v-if="series.length" id="series-list">
 			<h2>Series</h2>
 			<ul>
-				<li v-for="serie in series" :key="serie.id">{{ serie.name }}</li>
+				<li class="card" v-for="serie in series" :key="serie.id">
+					<div>{{ serie.name }}</div>
+					<div>{{ serie.original_name }}</div>
+					<div>{{ serie.original_language }}</div>
+					<div>{{ serie.vote_average }}</div>
+				</li>
 			</ul>
 		</section>
 	</main>
