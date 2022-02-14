1f403c12a536896ea9d9356dc30a55eea7d6cbc7
lindabettini <linda.bettini@hotmail.it>
2022-02-14 23:13:00 +0100

    add v-if at sections

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,12 @@
 <template>
 	<main>
-		<section id="movies-list">
+		<section v-if="movies.length" id="movies-list">
 			<h2>Movies</h2>
 			<ul>
 				<li v-for="movie in movies" :key="movie.id">{{ movie.title }}</li>
 			</ul>
 		</section>
-		<section id="series-list">
+		<section v-if="series.length" id="series-list">
 			<h2>Series</h2>
 			<ul>
 				<li v-for="serie in series" :key="serie.id">{{ serie.name }}</li>
