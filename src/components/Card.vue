d63f4c24c1504ef3bd5c1a3bf46d4e821982b5d1
lindabettini <linda.bettini@hotmail.it>
2022-02-15 11:44:28 +0100

    add component "Card"

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="row text-center">
+		<ul class="card col-3">
+			<li>{{ item.title }}</li>
+			<li>{{ item.original_title }}</li>
+			<li>{{ item.original_language }}</li>
+			<li>{{ item.vote_average }}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Card",
+	props: ["item"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,14 +6,8 @@
 		<div v-else>
 			<section v-if="movies.length" id="movies-list">
 				<h2>Movies</h2>
-				<div class="row text-center">
-					<ul class="card col-3" v-for="movie in movies" :key="movie.id">
-						<li>{{ movie.title }}</li>
-						<li>{{ movie.original_title }}</li>
-						<li>{{ movie.original_language }}</li>
-						<li>{{ movie.vote_average }}</li>
-					</ul>
-				</div>
+
+				<Card v-for="movie in movies" :key="movie.id" :item="movies" />
 			</section>
 			<section v-if="series.length" id="series-list">
 				<h2>Series</h2>
@@ -31,8 +25,10 @@
 </template>
 
 <script>
+import Card from "../components/Card.vue";
 export default {
 	name: "Main",
+	components: { Card },
 	props: ["movies", "series"],
 };
 </script>
