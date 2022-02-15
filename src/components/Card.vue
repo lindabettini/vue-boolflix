f3acbe9984120ce3b4ab9a1c8cd04fa286cebcfe
lindabettini <linda.bettini@hotmail.it>
2022-02-15 19:08:01 +0100

    add stars

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,8 +11,8 @@
 			<span v-else>{{ item.original_language }}</span>
 		</li>
 		<li>
-			{{ calculateStars() }}
-			<i class="fas fa-star"></i>
+			{{ calculateStars }}
+			<i v-for="(star, index) in calculateStars" :key="index" class="fas fa-star"></i>
 		</li>
 	</ul>
 </template>
@@ -30,30 +30,12 @@ export default {
 		flagSrc() {
 			return require(`../assets/img/${this.item.original_language}.png`);
 		},
-	},
-	methods: {
-		calculateStars(voteValue) {
-			voteValue = this.item.vote_average / 2;
-			return voteValue;
-		},
-
-		switchStars(voteValue) {
-			let stars;
-			if (voteValue.value <= 1) {
-				stars = 1;
-				return stars;
-			} else if (voteValue.value >= 1 || voteValue.value <= 2) {
-				stars = 2;
-				return stars;
-			} else if (voteValue.value >= 2 || voteValue.value <= 3) {
-				stars = 3;
-				return stars;
-			} else if (voteValue.value >= 3 || voteValue.value <= 4) {
-				stars = 4;
-				return stars;
-			} else voteValue.value >= 4 || voteValue.value <= 5;
-			stars = 5;
-			return stars;
+		calculateStars() {
+			let toPrint;
+			toPrint = this.item.vote_average;
+			toPrint = toPrint / 2;
+			toPrint = Math.ceil(toPrint);
+			return toPrint;
 		},
 	},
 };
